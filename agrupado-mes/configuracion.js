cdf2a2233bcb4d1dc9c6f63a726949de2e1d86ce
Alejandro <alejandroruperez@gmail.com>
2020-02-04 15:44:37 +0100

    Some structure to the project

---
+++ agrupado-mes/configuracion.js
@@ -0,0 +1,9 @@
+var datosMes = {
+    meses: [ 'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre' ],
+    numeros: [ 76, 117, 126, 147, 101, 111, 104, 86, 47, 54, 57, 50 ],
+    configuracionGrafico: {
+        titulo: 'Ag. por mes (2019)',
+        colorFondo: 'rgba(255, 0, 0, 0.3)',
+        colorBorde: 'rgba(255, 99, 132, 1)'
+    }
+};
