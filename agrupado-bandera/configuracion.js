48226a3d0e50cc62849ea900c80fcdfdb5aabfcd
Alejandro <alejandroruperez@gmail.com>
2020-02-05 16:58:03 +0100

    Added the horizontal bar chart

---
+++ agrupado-bandera/configuracion.js
@@ -0,0 +1,9 @@
+var datosMes = {
+    meses: [ 'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre' ],
+    numeros: [ 147, 126, 117, 111, 104, 101, 86, 76, 57, 54, 50, 47 ],
+    configuracionGrafico: {
+        titulo: 'Ag. por bandera (2019)',
+        colorFondo: 'rgba(255, 0, 0, 0.3)',
+        colorBorde: 'rgba(255, 99, 132, 1)'
+    }
+};
