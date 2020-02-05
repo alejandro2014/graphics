c83ab61a8193c1b4ae4f6b54250b296c97b84139
Alejandro <alejandroruperez@gmail.com>
2020-02-05 16:07:30 +0100

    Separated map configuration to another file

---
+++ mapa/map-configuration.js
@@ -0,0 +1,13 @@
+var provincesInfo = [
+    { name: 'madrid', value: 160 },
+    { name: 'barcelona', value: 140 },
+    { name: 'cuenca', value: 80 },
+    { name: 'valladolid', value: 70 },
+    { name: 'lugo', value: 40 },
+    { name: 'cadiz', value: 10 }
+];
+
+var tableOfColors = [
+    '#FDC5BF', '#ED9793', '#DD6968', '#CD3B3D', '#BE0E12',
+    '#A70D10', '#900C0F', '#790C0E', '#620B0D', '#4C0B0C'
+]
