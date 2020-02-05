02fbc401a30ab901f70ed4490352d07a3b8ccf09
Alejandro <alejandroruperez@gmail.com>
2020-02-05 15:56:33 +0100

    Added a little animation to the colouring of the provinces

--- mapa/spain-map.js
+++ mapa/spain-map.js
@@ -1,17 +1,3 @@
-var provincesInfo = [
-    { name: 'madrid', value: 160 },
-    { name: 'barcelona', value: 140 },
-    { name: 'cuenca', value: 80 },
-    { name: 'valladolid', value: 70 },
-    { name: 'lugo', value: 40 },
-    { name: 'cadiz', value: 10 }
-];
-
-var tableOfColors = [
-    '#FDC5BF', '#ED9793', '#DD6968', '#CD3B3D', '#BE0E12',
-    '#A70D10', '#900C0F', '#790C0E', '#620B0D', '#4C0B0C'
-]
-
 function getColorFromNumber(provinceValue) {
     var position = provinceValue < 145 ? Math.floor(provinceValue / 16) : 9;
 
@@ -46,9 +32,9 @@ function getProvinceColor(provinceName) {
 
             var path = R.path(province.path).attr(attr);
 
-            path.attr({
+            path.animate({
                 fill: getProvinceColor(province.name)
-            });
+            }, 2000);
         }
 
         mapBBox = { x: 151, y: 11, width: 417, height: 348 };
